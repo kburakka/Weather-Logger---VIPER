10d6fed25854bf536617e2a2905d53a589c2dffa
burak kaya <burak95kaya@hotmai.com>
2019-10-02 19:33:59 +0300

    Initial Commit

---
+++ Weather Logger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Logger
+//
+//  Created by burak kaya on 02/10/2019.
+//  Copyright © 2019 burak kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
